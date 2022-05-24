ead8d25ea84294516a4035dd7266cbdf21bf12d7
Anurag Gupta <anurag281996@gmail.com>
2022-05-24 16:57:27 -0600

    Time: 0 ms (100.00%), Space: 8.4 MB (25.05%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int z=0, t=n-1, o=0;
+        while(o<=t) {
+            if(nums[o]==1) {
+                o++;
+            } else if(nums[o]==0) {
+                swap(nums[z++], nums[o++]);
+            } else if(nums[o]==2) {
+                swap(nums[t--], nums[o]);
+            }
+        }
+    }
+};
