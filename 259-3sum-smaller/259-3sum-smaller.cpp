fb2bfca7bc004af15368e106f8a8acf73c204a42
Anurag Gupta <anurag281996@gmail.com>
2022-06-02 14:17:44 -0600

    Time: 63 ms (75.40%), Space: 13.6 MB (67.41%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.cpp
+++ 259-3sum-smaller/259-3sum-smaller.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int threeSumSmaller(vector<int>& nums, int target) {
-        int count=0, n=nums.size();
+        int count=0, sum=0, n=nums.size();
         if(n<3)
             return count;
+        
         sort(nums.begin(), nums.end());
-        int sum=0;
+        
         for(int i=0; i<n-2; i++) {
-            int l=i+1, r=n-1;
-            while(l<r) {
-                sum=nums[i]+nums[l]+nums[r];
-                if(sum>=target)
-                    r--;
-                else {
-                    count+=r-l;
-                    l++;
+            int start=i+1, end=n-1;
+            while(start<end) {
+                sum = nums[i]+nums[start]+nums[end];
+                if(sum<target) {
+                    count += end-start;
+                    start++;
+                } else {
+                    end--;
                 }
             }
         }
