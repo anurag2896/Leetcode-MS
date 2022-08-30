0e8c75da1faf6e4f9e37dfd3ea746528196654c7
Anurag Gupta <anurag281996@gmail.com>
2022-08-30 14:38:12 -0600

    Time: 30 ms (25.02%), Space: 13.2 MB (82.00%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int firstRow=1, n=matrix.size(), m=matrix[0].size();
-        // traversing all but the first column
-        
-        if(matrix[0][0]==0)
-            firstRow = 0;
-        
+        int fr = 1, n = matrix.size(), m=matrix[0].size();
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(i==0 && j==0)
-                    continue;
                 if(matrix[i][j]==0) {
-                    if(i==0)
-                        firstRow = 0;
-                    else if(i!=0 || j!=0) {
-                        matrix[i][0]=0;
+                    if(i==0) {
+                        fr=0;
+                    } else {
                         matrix[0][j]=0;
-                    }   
-                }
+                        matrix[i][0]=0;
+                    }
+                } 
             }
         }
         
-        // column wise
+        // cout<<"printing"<<endl ;
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<m; j++) {
+        //         cout<<matrix[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        //column wise
         for(int i=1; i<m; i++) {
-            // cout<<"here"<<i<<" "<<n<<" - ";
             if(matrix[0][i]==0) {
-                // cout<<"inside col"<<i<<endl;
                 for(int j=1; j<n; j++) {
                     matrix[j][i]=0;
                 }
             }
         }
         
-        // row wise
+        //row wise
         for(int i=1; i<n; i++) {
             if(matrix[i][0]==0) {
                 for(int j=1; j<m; j++) {
@@ -42,17 +41,32 @@ public:
             }
         }
         
+        // cout<<"printing"<<endl ;
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<m; j++) {
+        //         cout<<matrix[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
         if(matrix[0][0]==0) {
-            for(int i=1; i<n; i++) {
+            for(int i=0; i<n; i++) {
                 matrix[i][0]=0;
             }
         }
         
-        if(firstRow==0) {
+        if(fr==0) {
             for(int i=0; i<m; i++) {
                 matrix[0][i]=0;
             }
         }
         
+        
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<m; j++) {
+        //         cout<<matrix[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
     }
 };
