639b84998ca0f4828daa726a1bdb61eddc7ea373
Anurag Gupta <anurag281996@gmail.com>
2022-08-31 18:27:12 -0600

    Time: 4 ms (41.29%), Space: 7.1 MB (33.77%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    void transpose(vector<vector<int>>& matrix) {
+    void transpose(vector<vector<int>> &matrix) {
         int n=matrix.size();
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 if(i<j) {
                     swap(matrix[i][j], matrix[j][i]);
                 }
-                // if(j<=i)
-                //     swap(matrix[i][j], matrix[j][i]);
             }
         }
     }
     
-    void ultapulta(vector<vector<int>>& matrix) {
+    void mirror(vector<vector<int>>& matrix) {
         int n=matrix.size();
         for(int i=0; i<n; i++) {
             for(int j=0; j<n/2; j++) {
@@ -22,19 +20,8 @@ public:
         }
     }
     
-    void print(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                cout<<matrix[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-    }
-    
     void rotate(vector<vector<int>>& matrix) {
         transpose(matrix);
-        // print(matrix);
-        ultapulta(matrix);
+        mirror(matrix);
     }
 };
