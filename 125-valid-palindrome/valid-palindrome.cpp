db0dfa0965464e7ff8749c3adb963b52083d9b3e
Anurag Gupta <anurag281996@gmail.com>
2024-09-21 15:06:30 -0600

    Time: 9 ms (22.38%) | Memory: 8.9 MB (84.33%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start=0, end=s.size()-1;
+        while(start<end) {
+            while(start<end && !isalnum(s[start]))
+                start++;
+            while(start<end && !isalnum(s[end]))
+                end--;
+
+            if(start<end && tolower(s[start])!=tolower(s[end]))
+                return false;
+            
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
