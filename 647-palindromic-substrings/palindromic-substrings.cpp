15f5f5b368055d8b03af2ee4f267f6ca1c46e000
Anurag Gupta <anurag281996@gmail.com>
2024-09-27 00:04:28 -0600

    Time: 15 ms (48.88%) | Memory: 21.7 MB (34.55%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+
+    int helper(string s, int l, int r) {
+        int n=s.size(), count=0;
+        while(l>=0 && r<n && s[l]==s[r]) {
+            l--;
+            r++;
+            count++;
+        }
+        return count;
+    }
+
+public:
+    int countSubstrings(string s) {
+        int n=s.size();
+        int count=0;
+
+        for(int i=0; i<n; i++) {
+            count+=helper(s, i, i);
+            count+=helper(s, i, i+1);
+        }
+        return count;
+    }
+};
