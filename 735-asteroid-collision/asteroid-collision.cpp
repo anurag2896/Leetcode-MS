12dc5d2661b0a40bdc62ba6dd84b670d47adf28e
Anurag Gupta <anurag281996@gmail.com>
2024-09-29 12:16:17 -0600

    Time: 7 ms (84.91%) | Memory: 20.8 MB (65.85%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -4,22 +4,26 @@ public:
         stack<int> s;
         int i=0;
         while(i<asteroids.size()) {
+            // if incoming is greater and clashing then keep popping from existing
             while(!s.empty() && s.top()>0 && asteroids[i]<0 && abs(asteroids[i]) > abs(s.top())) {
                 s.pop();
                 // i++;
             }
 
+            // if incoming is smaller and clashing then dont consider it
             if(!s.empty() && s.top()>0 && asteroids[i]<0 && abs(asteroids[i]) < abs(s.top())) {
                 i++;
                 continue;
             }
 
+            // if incoming is same and clashing then remove both
             if(!s.empty() && s.top()>0 && asteroids[i]<0 && abs(s.top())==abs(asteroids[i])) {
                 s.pop();
                 i++;
                 continue;
             }
 
+            // if incoming is is same size and and same direcrtino or opposite direction but not clashing then consider it
             if(s.empty() || (s.top()>0 && asteroids[i]>0) || (s.top()<0 && asteroids[i]<0) || (s.top()<0 && asteroids[i]>0 )) {
                 s.push(asteroids[i]);
             }
