4166aba03d26e62ea376c1262ee15bd7a4b7d11b
Anurag Gupta <anurag281996@gmail.com>
2024-09-29 14:06:09 -0600

    Time: 0 ms (100.00%) | Memory: 8.7 MB (93.59%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> m(26, 0);
+        for(int i=0; i<s.size(); i++) {
+            m[s[i]-'a']++;
+        }
+        for(int i=0; i<t.size(); i++) {
+            m[t[i]-'a']--;
+        }
+
+        for(int i=0; i<26; i++) {
+            if(m[i]!=0)
+                return false;
+        }
+        return true;
+    }
+};
