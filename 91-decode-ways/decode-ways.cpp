16222499133e59496146ff38bbcc1d31e082be69
Anurag Gupta <anurag281996@gmail.com>
2024-10-03 12:09:15 -0600

    Time: 9 ms (5.68%) | Memory: 13.1 MB (5.28%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,29 +1,42 @@
 class Solution {
 
-    int dfs(string s, int i, vector<int>& dp) {
+    int helper(string s, unordered_map<string, char>& m, int index, vector<int>& dp) {
         int n=s.size();
 
-        if(i==n) {
+        if(index==n) {
             return 1;
         }
-        if(s[i]=='0')
+
+        if(index > n)
             return 0;
 
-        if(dp[i]!=-1)
-            return dp[i];
+        if(dp[index]!=-1)
+            return dp[index];
 
-
-        int takeOne = dfs(s, i+1, dp);
+        int takeOne = 0;
+        if(m.find(s.substr(index, 1))!=m.end()) {
+            takeOne = helper(s, m, index+1, dp);
+        }
         int takeTwo = 0;
-        if(i+2 <= n && (s[i] == '1' || (s[i] == '2' && s[i+1] < '7')))
-            takeTwo = dfs(s, i+2, dp);
+        if(m.find(s.substr(index, 2))!=m.end()) {
+            takeTwo = helper(s, m, index+2, dp);
+        }
 
-        return dp[i] = takeOne + takeTwo;
+        return dp[index] = takeOne + takeTwo;
     }
 
 public:
     int numDecodings(string s) {
-        vector<int> dp(s.size(), -1);
-        return dfs(s, 0, dp);
+        unordered_map<string, char> m;
+        int n=s.size();
+
+        vector<int> dp(n, -1);
+        for(int i=0; i<26; i++) {
+            m[to_string(i+1)] = 'A'+i;
+        }
+
+        int ans =0;
+        ans = helper(s, m, 0, dp);
+        return ans;
     }
 };
