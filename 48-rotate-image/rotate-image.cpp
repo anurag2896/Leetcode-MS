8b5935b211e07e1c56349657ecf5bcf554709eab
Anurag Gupta <anurag281996@gmail.com>
2024-10-02 22:28:59 -0600

    Time: 5 ms (39.68%) | Memory: 9 MB (58.36%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,26 +1,30 @@
 class Solution {
-    void swapDiag(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+
+    // along main diagonal
+    void transpose(vector<vector<int>>& matrix) {
+        int n=matrix.size();
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(i<j)
+                if(i>j) {
                     swap(matrix[i][j], matrix[j][i]);
+                }
             }
         }
     }
 
-    void swapHor(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        for(int i=0; i<n/2; i++) {
-            for(int j=0; j<n; j++) {
-                swap(matrix[j][i], matrix[j][n-1-i]);
+    // swap vertically
+    void mirror(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n/2; j++) {
+                swap(matrix[i][j], matrix[i][n-1-j]);
             }
         }
     }
 
 public:
     void rotate(vector<vector<int>>& matrix) {
-        swapDiag(matrix);
-        swapHor(matrix);
+        transpose(matrix);
+        mirror(matrix);
     }
 };
