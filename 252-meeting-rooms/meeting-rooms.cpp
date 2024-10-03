5ad98333f218bc8546d64b7ef93b8823003c598e
Anurag Gupta <anurag281996@gmail.com>
2024-10-02 23:11:05 -0600

    Time: 7 ms (89.30%) | Memory: 16.1 MB (13.07%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.cpp
+++ 252-meeting-rooms/meeting-rooms.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
-        int rooms=0;
-        map<int, int> m;
-        for(auto i:intervals) {
-            m[i[0]]++;
-            m[i[1]]--;
-        }
+        if(intervals.empty())
+            return true;
+        sort(intervals.begin(), intervals.end());
 
-        int count=0;
-        for(auto ele:m) {
-            count+=ele.second;
-            rooms = max(rooms, count);
+        int start=INT_MIN, end=INT_MAX;
+        for(int i=0; i<intervals.size()-1; i++) {
+            auto curr = intervals[i];
+            auto next = intervals[i+1];
+
+            int maxStart = max(curr[0], next[0]);
+            int minEnd = min(curr[1], next[1]);
+
+            if(minEnd > maxStart)
+                return false;
         }
-        
-        if(rooms > 1)
-            return false;
-        
         return true;
     }
 };
