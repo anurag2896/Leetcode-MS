450052c6a1651317a50e34ea12fe880aba506d95
Anurag Gupta <anurag281996@gmail.com>
2024-10-03 14:20:27 -0600

    Time: 4 ms (13.10%) | Memory: 7.9 MB (52.47%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+
+    int helper(int m, int n, vector<vector<int>>& dp) {
+        if(m==0 && n==0)
+            return 1;
+        
+        if(m<0 || n<0)
+            return 0;
+
+        if(dp[m][n]!=-1)
+            return dp[m][n];
+
+        int top = helper(m-1, n, dp);
+        int left = helper(m, n-1, dp);
+
+        return dp[m][n]=top+left;
+    }
+
+public:
+    int uniquePaths(int m, int n) {
+        int ways=0;
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+
+        return helper(m-1, n-1, dp);
+    }
+};
