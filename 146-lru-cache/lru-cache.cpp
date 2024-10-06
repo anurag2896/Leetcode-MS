804606beb3c3f9a9a19bc0e1738f8b2500ba7101
Anurag Gupta <anurag281996@gmail.com>
2024-10-06 15:53:20 -0600

    Time: 310 ms (97.51%) | Memory: 169.9 MB (84.12%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,7 +1,8 @@
 class LRUCache {
-    unordered_map<int, list<pair<int, int>>::iterator> m;
+
     int size;
-    list<pair<int, int>> dll;
+    list<pair<int, int>> dll;   //key, val
+    unordered_map<int, list<pair<int, int>>::iterator> m;   //key, iterator
 
 public:
     LRUCache(int capacity) {
@@ -10,9 +11,9 @@ public:
     
     int get(int key) {
         auto found = m.find(key);
-        if(found==m.end()) {
+        if(found==m.end())
             return -1;
-        }
+        
         dll.splice(dll.begin(), dll, found->second);
         m[key] = dll.begin();
         return found->second->second;
@@ -21,19 +22,21 @@ public:
     void put(int key, int value) {
         auto found = m.find(key);
         if(found!=m.end()) {
+            found->second->second = value;
             dll.splice(dll.begin(), dll, found->second);
             m[key] = dll.begin();
-            found->second->second = value;
             return;
         }
-        if(dll.size() == size) {
-            int keyTobeDel = dll.back().first;
-            m.erase(keyTobeDel);
+
+        if(dll.size()==size) {
+            auto keyToDel = dll.back().first;
+            m.erase(keyToDel);
             dll.pop_back();
         }
 
         dll.push_front({key, value});
         m[key] = dll.begin();
+        return;
     }
 };
 
