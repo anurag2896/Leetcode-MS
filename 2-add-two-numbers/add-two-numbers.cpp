730a4fd8c5791dd4373c09f23c9acfa85c77fad7
Anurag Gupta <anurag281996@gmail.com>
2024-10-05 23:19:44 -0600

    Time: 30 ms (19.78%) | Memory: 77.6 MB (43.54%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,25 +11,28 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int sum=0, carry=0;
+        int u=0, v=0, carry=0;
+
         ListNode* newHead = new ListNode(), *temp;
         temp = newHead;
+
         while(l1 || l2 || carry) {
-            int val1 = l1 ? l1->val : 0;
-            int val2 = l2 ? l2->val : 0;
+            u = l1 ? l1->val : 0;
+            v = l2 ? l2->val : 0;
 
-            if(l1) 
-                l1 = l1->next;
-            if(l2) 
-                l2 = l2->next;
+            if(l1)
+                l1=l1->next;
+            if(l2)
+                l2=l2->next;
 
-            int sum  = val1 + val2 + carry;
+            int sum = u+v+carry;
             carry = sum/10;
-            sum %= 10;
+            sum%=10;
 
             temp->next = new ListNode(sum);
             temp = temp->next;
         }
+
         return newHead->next;
     }
 };
