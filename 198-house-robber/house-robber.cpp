ac74a899829d978ce46f0d93dba43e0da1e6e8ce
Anurag Gupta <anurag281996@gmail.com>
2024-10-06 16:12:56 -0600

    Time: 0 ms (100.00%) | Memory: 9.4 MB (63.40%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,26 +1,30 @@
 class Solution {
 
-    int helper(vector<int>& nums, vector<int>& dp, int n) {
-        if(n<0)
-            return 0;
+    int helper(int index, vector<int>& nums, vector<int>& dp) {
+        if(index<0)
+            return INT_MIN;
         
-        if(n==0)
-            return dp[n] = nums[0];
+        if(index==0)
+            return nums[0];
 
-        if(dp[n]!=-1)
-            return dp[n];
+        if(dp[index]!=-1)
+            return dp[index];
+        
+        int notTake = helper(index-1, nums, dp);
+        int take = nums[index];
+        if(index-2>=0)
+            take += helper(index-2, nums, dp);
 
-        int take = nums[n] + helper(nums, dp, n-2);
-        int nottake = helper(nums, dp, n-1);
+        return dp[index] = max(take, notTake);
 
-        return dp[n] = max(take, nottake);
+
     }
 
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n, -1);
-        helper(nums, dp, n-1);
-        return dp[n-1];
+
+        return helper(n-1, nums, dp);
     }
 };
