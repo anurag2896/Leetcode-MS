0019cf0172975460f274bcf378bb8d9f43b86857
Anurag Gupta <anurag281996@gmail.com>
2024-10-05 23:02:42 -0600

    Time: 39 ms (20.55%) | Memory: 39.6 MB (23.07%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -8,39 +8,31 @@
  * };
  */
 class Codec {
-
-    string serializeUtil(TreeNode* root) {
-        if(!root)
-            return "#";
-        
-        return to_string(root->val) + "," + serializeUtil(root->left) + "," + serializeUtil(root->right);
-    }
-
-    TreeNode* deserializeUtil(stringstream& ss, string temp) {
-        getline(ss, temp, ',');
-        if(temp=="#")
-            return NULL;
-
-        TreeNode* root = new TreeNode(stoi(temp));
-        root->left = deserializeUtil(ss, temp);
-        root->right = deserializeUtil(ss, temp);
-        return root;
-    }
-
 public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        return serializeUtil(root);
+        if(!root)
+            return "#";
+        return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
+    }
+
+    TreeNode* helper(stringstream& ss) {
+        string temp;
+        getline(ss, temp, ',');
+        if(temp=="#")
+            return NULL;
+
+        TreeNode* node = new TreeNode(stoi(temp));
+        node->left = helper(ss);
+        node->right = helper(ss);
+        return node;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        TreeNode* root;
-        
         stringstream ss(data);
-        string temp;
-        return deserializeUtil(ss, temp);
+        return helper(ss);
     }
 };
 
